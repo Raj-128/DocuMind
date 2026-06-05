60fa2fbb91e03f866ed0a9dfe3b8e191bdcdc9fe
Raj-128 <rajpatil052003@gmail.com>
2026-06-05 20:29:51 +0530

    Update settings.py

--- Documind/settings.py
+++ Documind/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 if not SECRET_KEY:
     raise ValueError("No SECRET_KEY set for Django application")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
